c9a2f14e44a8a33d4256130c1e2a230803557a14
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-16 20:02:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,7 +25,7 @@ def game_hash
                   :away => {
                 :team_name => "Charlotte Hornets",
                 :colors => ["Turquoise", "Purple"],
-                :players => [{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,
+                :players => [{:player_name => "Jeff Adrien", :number => 0, :shoe => 16, :points => 22,
                               :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
                               
                              {:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12,
