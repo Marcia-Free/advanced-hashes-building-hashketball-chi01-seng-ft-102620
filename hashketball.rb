1e6aef4574766e6bab0fd34b48a55556e19c6e89
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-16 19:54:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,11 @@ def game_hash
   hashketball = { :home => {
                 :team_name => "Brooklyn Nets",
                 :colors => ["Black", "White"],
-                :players => [{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,      :rebounds
+                :players => [{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,
+                              :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, slam_dunks => 1},
+                             {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,
+                              :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, slam_dunks => 1},
+              
                 
                 }]
   }, 
